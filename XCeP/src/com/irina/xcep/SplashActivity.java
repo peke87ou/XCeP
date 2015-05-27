768b9aaffcd12607fe040e3e05625e3ab41498c2
0011530 <0011530@PB2T9GW.insags.com>
2015-05-27 17:50:07 +0200

    Login versión parse

--- XCeP/src/com/irina/xcep/SplashActivity.java
+++ XCeP/src/com/irina/xcep/SplashActivity.java
@@ -1,36 +1,60 @@
 package com.irina.xcep;
 
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseUser;
 
 
-public class SplashActivity extends ActionBarActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+       // setContentView(R.layout.activity_splash);
+     // Determine whether the current user is an anonymous user
+     		if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+     			// If user is anonymous, send the user to LoginSignupActivity.class
+     			Intent intent = new Intent(SplashActivity.this,LoginSignupActivity.class);
+     			startActivity(intent);
+     			finish();
+     		} else {
+     			// If current user is NOT anonymous user
+     			// Get current user data from Parse.com
+     			ParseUser currentUser = ParseUser.getCurrentUser();
+     			if (currentUser != null) {
+     				// Send logged in users to Welcome.class
+     				Intent intent = new Intent(SplashActivity.this, Welcome.class);
+     				startActivity(intent);
+     				finish();
+     			} else {
+     				// Send user to LoginSignupActivity.class
+     				Intent intent = new Intent(SplashActivity.this,LoginSignupActivity.class);
+     				startActivity(intent);
+     				finish();
+     			}
+     		}
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.splash, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
