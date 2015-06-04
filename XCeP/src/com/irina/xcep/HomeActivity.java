4ff7abb3d425e88bb0313ff553c4adfbc1506aa5
0011530 <0011530@PB2T9GW.insags.com>
2015-06-04 18:34:44 +0200

    Modificación de home

--- XCeP/src/com/irina/xcep/HomeActivity.java
+++ XCeP/src/com/irina/xcep/HomeActivity.java
@@ -1,20 +1,22 @@
 package com.irina.xcep;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.w3c.dom.Comment;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gc.materialdesign.views.ButtonRectangle;
+import com.irina.xcep.adapters.AdapterListas;
 import com.irina.xcep.model.Lista;
-import com.irina.xcep.model.Supermercado;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -25,6 +27,12 @@ public class HomeActivity extends Activity {
 	
 	// Declare Variable
 	ButtonRectangle logout;
+	ListView list;
+	List<ParseObject> ob;
+	AdapterListas adapter;
+	ArrayList<Lista> misListas = new ArrayList<Lista>();
+	ImageButton addlist;
+	
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -57,24 +65,62 @@ public class HomeActivity extends Activity {
 			}
 		});
 		
+		list = (ListView) findViewById(R.id.lista_list);
+		adapter = new AdapterListas(HomeActivity.this, misListas);//ArrayAdapter<Lista>(HomeActivity.this,R.layout.item_shopping_list);
+		list.setAdapter(adapter);
+		
 		//Supermercado market =  new Supermercado();
 		//market.setNome("Mercadona");
+		
 		ParseQuery<Lista> query = ParseQuery.getQuery(Lista.class);
 		query.findInBackground(new FindCallback<Lista>() {
 			
 			@Override
 			public void done(List<Lista> objects, ParseException e) {
-				// TODO Auto-generated method stub
+				
+
+				misListas = (ArrayList<Lista>) objects;
+				
+				adapter.clear();
+				adapter.addAll(misListas);
+				//adapter.setNotifyOnChange(true); Forzar a que se vuelva dibujar la lista asociada a este adapter
+				
 				//market.getNome();
-				TextView txtsupermarket = (TextView) findViewById(R.id.txtsupermarket);
-				txtsupermarket.setText(objects.get(0).getNome());
+				/*for (int i =0; i < objects.size(); i++){
+					LinearLayout lista = (LinearLayout) getLayoutInflater().inflate(R.layout.item_shopping_list, null);
+					((TextView) lista.findViewById(R.id.name_list)).setText(objects.get(0).getNome());
+					// ((ViewGroup) list.getParent()).addView(lista);
+					list.addView(lista);
+				}*/
+				//TextView txtsupermarket = (TextView) findViewById(R.id.txtsupermarket);
+				//txtsupermarket.setText();
 				Log.i("Lista", objects.get(0).getIdSupermercado()+"");
 				Log.i("Lista", objects.get(0).getProductos()+"");
 //				Log.i("Supermercado", objects.get(0).getUrlLogo().+"");
 			}
 		});
 		
+		
+		addlist = (ImageButton) findViewById(R.id.add_list);
+		
+		addlist.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//Ir a páxina de rexistro
+//				Intent intent = new Intent(HomeActivity.this, SignupActivity.class);
+//				startActivity(intent);
 				
+				Toast.makeText(
+						getApplicationContext(),
+						"Ventana de añadir listas",
+						Toast.LENGTH_LONG).show();
+				
+			}
+		});
 
+		
+		
+	
 	}
 }
---
+++ XCeP/src/com/irina/xcep/adapters/AdapterListas.java
@@ -0,0 +1,42 @@
+package com.irina.xcep.adapters;
+
+import java.util.ArrayList;
+
+import com.irina.xcep.R;
+import com.irina.xcep.model.Lista;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AdapterListas extends ArrayAdapter<Lista> {
+    
+	public AdapterListas(Context context, ArrayList<Lista> lista) {
+       super(context, 0, lista);
+    }
+
+    @Override
+    public View getView(int position, View celdaView, ViewGroup parent) {
+       
+    	// Get the data item for this position
+       Lista lista = getItem(position);    
+       
+       // Check if an existing view is being reused, otherwise inflate the view
+       if (celdaView == null) {
+          celdaView = LayoutInflater.from(getContext()).inflate(R.layout.item_shopping_list, parent, false);
+       }
+       
+       // Lookup view for data population
+       ((TextView) celdaView.findViewById(R.id.name_list)).setText(lista.getNome());
+//       ((TextView) celdaView.findViewById(R.id.products_list)).setText(lista.getProductos().size()+ R.string.products);
+       //((ImageView) celdaView.findViewById(R.id.imageMarketList)).set(lista.getIdSupermercado().getQuery().getClass().getMethods().);
+       
+       
+       
+       return celdaView;
+   }
+}
