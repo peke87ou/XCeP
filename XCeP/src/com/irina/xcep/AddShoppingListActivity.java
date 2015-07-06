a65359e52e7c3ec9dc1432b4cac091a1e2972f95
0011530 <0011530@PB2T9GW.insags.com>
2015-07-06 15:00:19 +0200

    Modificamos los fallos de añadir nueva lista

--- XCeP/src/com/irina/xcep/AddShoppingListActivity.java
+++ XCeP/src/com/irina/xcep/AddShoppingListActivity.java
@@ -3,10 +3,9 @@ package com.irina.xcep;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -18,12 +17,14 @@ import com.gc.materialdesign.views.ButtonRectangle;
 import com.irina.xcep.adapters.AdapterGridAddShoppingList;
 import com.irina.xcep.model.Lista;
 import com.irina.xcep.model.Supermercado;
+import com.irina.xcep.utils.Utils;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+@SuppressLint("NewApi")
 public class AddShoppingListActivity extends Activity{
 	
 	ButtonRectangle btncancel, btnacept;
@@ -88,28 +89,35 @@ public class AddShoppingListActivity extends Activity{
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
               
         	// Esta parte la uso para poder quitar el fondo cambiado a la fila seleccionada anteriormente
-        	if(grid != null){
-        		if(!grid.equals(view)){
-//        			view.getBackground();
-//        	        grid.setBackgroundDrawable(view.getBackground());
-        	      
-        	        Log.i("1", "por aki");
-        	    }                            
-        	 } else{
-        		 grid.setItemChecked(0, true);
-        		
-        	 }
-        	
-        	if(!click_item){
-	        	// Aqui cambio o fondo da fila seleccionada actualmente
-	        	view.setBackgroundColor(Color.argb(200,208,245,169));  
-	        	click_item = true;
-	        	Log.i("2", parent.getItemAtPosition(position).toString());
-        	}else{
-        		view.setBackgroundColor(Color.TRANSPARENT); 
-        		click_item = false;
-        	}
-        	Toast.makeText(AddShoppingListActivity.this, "You Clicked at " + supermercados.get(position), Toast.LENGTH_SHORT).show();
+//        	if(grid != null){
+//        		if(!grid.equals(view)){
+////        			view.getBackground();
+////        	        grid.setBackgroundDrawable(view.getBackground());
+////        			if(!click_item){
+////        	        	// Aqui cambio o fondo da fila seleccionada actualmente
+////        	        	view.setBackgroundColor(Color.argb(200,208,245,169));  
+////        	        	click_item = true;
+////        	        	Log.i("2", parent.getItemAtPosition(position).toString());
+////                	}else{
+////                		view.setBackgroundColor(Color.TRANSPARENT); 
+////                		click_item = false;
+////                	}
+//        	      //  Log.i("1", "por aki");
+//        			grid.setChoiceMode(GridView.CHOICE_MODE_SINGLE);
+//        			 if (grid.getSelectedItem() != null) {
+//        	                view.setBackgroundColor(Color.GREEN);
+//        	            } else {
+//        	                view.setBackgroundColor(Color.BLUE);
+//        	            }
+//        	    }                            
+//        	 } else{
+//        		 grid.setItemChecked(0, true);
+//        		 click_item = true;
+//        		
+//        	 }
+        	grid.setChoiceMode(GridView.CHOICE_MODE_SINGLE);
+        	//view.setBackground(getResources().getDrawable(R.drawable.grid_selector));
+        	//Toast.makeText(AddShoppingListActivity.this, "You Clicked at " + supermercados.get(position), Toast.LENGTH_SHORT).show();
         	idSuper = supermercados.get(position);
              }
          });
@@ -124,21 +132,33 @@ public class AddShoppingListActivity extends Activity{
 		//Nome da lista
 		nameList = (EditText) findViewById(R.id.text_name_list);
 		nameListtxt = nameList.getText().toString();
+		
+		
+		boolean allfilled = true;
+		allfilled =  Utils.isNotEmpty(nameList, nameListtxt);
+		if(!allfilled) return;
+		
 		addlist.setNome(nameListtxt);
 		
 		//Id supermercado seleccionado
-		addlist.setIdSupermercado(idSuper);
+		if (idSuper == null ){
+			Toast.makeText(AddShoppingListActivity.this, "No ha seleccionado ningún supermercado", Toast.LENGTH_SHORT).show();
+		}else{
+			addlist.setIdSupermercado(idSuper);
+			//Id usuario logeuado
+			addlist.setIdUser(ParseUser.getCurrentUser());
+			
+			addlist.saveInBackground(new SaveCallback() {
+				@Override
+				public void done(ParseException arg0) {
+					Toast.makeText(AddShoppingListActivity.this, "Engadimos a nova lista " + nameListtxt, Toast.LENGTH_SHORT).show();
+					finish();
+				}
+			});
+
+		}
 		
-		//Id usuario logeuado
-		addlist.setIdUser(ParseUser.getCurrentUser());
 		
-		addlist.saveInBackground(new SaveCallback() {
-			@Override
-			public void done(ParseException arg0) {
-				Toast.makeText(AddShoppingListActivity.this, "Engadimos a nova lista " + nameListtxt, Toast.LENGTH_SHORT).show();
-				finish();
-			}
-		});
 	}
 
 
