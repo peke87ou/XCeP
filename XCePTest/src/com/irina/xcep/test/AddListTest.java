0b206f0bd490068daa8349bc8d77974043745938
Peke <Peke@Peke_ou>
2015-07-20 21:48:34 +0200

    addlist

--- XCePTest/src/com/irina/xcep/test/AddListTest.java
+++ XCePTest/src/com/irina/xcep/test/AddListTest.java
@@ -5,38 +5,40 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.EditText;
 
-import com.irina.xcep.AddShoppingListActivity;
+import com.gc.materialdesign.views.ButtonRectangle;
+import com.irina.xcep.LoginActivity;
 import com.irina.xcep.R;
-import com.irina.xcep.test.enums.AddListEnum;
+import com.irina.xcep.test.enums.LoginEnum;
+import com.irina.xcep.test.enums.SignUpEnum;
 import com.robotium.solo.Solo;
 
-public class AddListTest extends ActivityInstrumentationTestCase2<AddShoppingListActivity> {
+public class AddListTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 	
 	private Solo solo;
-	private AddShoppingListActivity addlist;
+	private LoginActivity login;
 	private TestHelper helper;
-	private EditText namelist;
-	//private ButtonRectangle buttonlogin;
+	private EditText username,password;
+	private ButtonRectangle buttonlogin;
 	
 	static int TIME_OUT_LOGIN = 30000;
 
-	public AddListTest(Class<AddShoppingListActivity> activityClass) {
+	public AddListTest(Class<LoginActivity> activityClass) {
 		super(activityClass);
 	}
 
 	public AddListTest() {
-		this(AddShoppingListActivity.class);
+		this(LoginActivity.class);
 	}
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		solo= new Solo(getInstrumentation(), getActivity());
-		addlist = (AddShoppingListActivity) solo.getCurrentActivity();
+		login = (LoginActivity) solo.getCurrentActivity();
 		helper = new TestHelper(solo);
 		
-		namelist  = (EditText) addlist.findViewById(R.id.name_list);
-		
-		//buttonlogin = (ButtonRectangle) addlist.findViewById(R.id.login);
+		username  = (EditText) login.findViewById(R.id.username);
+		password  = (EditText) login.findViewById(R.id.password);
+		buttonlogin = (ButtonRectangle) login.findViewById(R.id.login);
 	}
 	
 	@Override
@@ -45,53 +47,55 @@ public class AddListTest extends ActivityInstrumentationTestCase2<AddShoppingLis
     }
 	
 	
-	public void tesAddListOk() {
+	public void testAddListOk() {
 		
-		helper.setAddListData(AddListEnum.CORRECTO);
+		helper.setLoginData(LoginEnum.CORRECTO);
 		
-		solo.enterText(namelist, helper.getNameList());
+		solo.enterText(username, helper.getUser());
+		solo.enterText(password, helper.getPass());
+		solo.clickOnView(buttonlogin);
 		
-//		solo.clickOnView(buttonlogin);
-		solo.searchText("As credecnciales son inválidas, se non está auténticado no sistema, por favor rexistrese");
+		helper.logout();
+
 	}
 	
 	
-//	public void testAddListNotName() {
-//		
-//		helper.setLoginData(LoginEnum.INCORRECTO);
-//		
-//		solo.enterText(namelist, helper.getUser());
-////		solo.enterText(password, helper.getPass());
-////		solo.clickOnView(buttonlogin);
-//		solo.searchText("As credecnciales son inválidas, se non está auténticado no sistema, por favor rexistrese");
-//	}
-//	
-//	public void testAddListNotMarket() {
-//		
-//		helper.setLoginData(LoginEnum.INCORRECTO);
-//		
-//		solo.enterText(namelist, helper.getUser());
-////		solo.enterText(password, helper.getPass());
-////		solo.clickOnView(buttonlogin);
-//		solo.searchText("As credecnciales son inválidas, se non está auténticado no sistema, por favor rexistrese");
-//	}
+	public void testAddListNotName() {
+		
+		helper.setLoginData(LoginEnum.INCORRECTO);
+		
+		solo.enterText(username, helper.getUser());
+		solo.enterText(password, helper.getPass());
+		solo.clickOnView(buttonlogin);
+		solo.searchText("As credecnciales son inválidas, se non está auténticado no sistema, por favor rexistrese");
+	}
+	public void testAddListNotMarket() {
+		
+		helper.setLoginData(LoginEnum.INCORRECTO);
+		
+		solo.enterText(username, helper.getUser());
+		solo.enterText(password, helper.getPass());
+		solo.clickOnView(buttonlogin);
+		solo.searchText("As credecnciales son inválidas, se non está auténticado no sistema, por favor rexistrese");
+	}
 	
-	public void tesAddListFailedEmpty() {
+	public void testAddListFailedEmpty() {
 		
-		helper.setAddListData(AddListEnum.INCORRECTO_EMPTY);
+		helper.setSignData(SignUpEnum.INCORRECTO_EMPTY);
 		
-		solo.enterText(namelist, helper.getUser());
+		solo.enterText(username, helper.getUserSign());
+		solo.enterText(password, helper.getPassSing());
+		solo.clickOnView(buttonlogin);
+		
 		//Utilizado para esperar por el retardo de las vistas en mostrar el error al estar vacias
 		try {
 			Thread.sleep(2500);
 		} catch (InterruptedException e) {
-			Log.e(AddListTest.class.getName(),"Interrupted Exception");
+			Log.e(SignUpTest.class.getName(),"Interrupted Exception");
 		}
 		
-		assertEquals(true, !TextUtils.isEmpty(namelist.getError().toString()) && namelist.getError().toString().equalsIgnoreCase("É necesario encher o campo seleccionado"));
+		assertEquals(true, !TextUtils.isEmpty(username.getError().toString()) && username.getError().toString().equalsIgnoreCase("É necesario encher o campo seleccionado"));
+		assertEquals(true, !TextUtils.isEmpty(password.getError().toString()) && password.getError().toString().equalsIgnoreCase("É necesario encher o campo seleccionado"));
 		
-		
-		solo.searchText("As credecnciales son inválidas, se non está auténticado no sistema, por favor rexistrese");
 	}
-
 }
